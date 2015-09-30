b0eb394e398c5a423c6fbb61c606673342f67f49
crussi <chrisrussi@gmail.com>
2015-09-30 12:59:03 -0700

    You ROCK!!! You got all selected states working :-)

--- client/components/browser.jsx
+++ client/components/browser.jsx
@@ -3,21 +3,26 @@ Browser = React.createClass({
     getInitialState() {
         return {
             path: [],
-            selectedId: ''
+            selected:[]
         }
     },
     navUp() {
-        this.setState({path: this.state.path.slice(0, -1)})
+        this.setState({path: this.state.path.slice(0, -1)});
+        this.setState({selected: this.state.selected.slice(0, -1)});
+        //??how set selectedId??
     },
     navDown(item,index) {
         console.log('browser navDown id: ' + item.id);
         if (item.children) {
             this.setState({path: this.state.path.concat(index)});
+            this.setState({selected: this.state.selected.slice(0,-1).concat(item.id).concat('')})
+        } else {
+            this.setState({selected: this.state.selected.slice(0,-1).concat(item.id)})
         }
-        this.setState({selectedId: item.id});
     },
     render() {
         const {path} = this.state;
+        let selectedId = this.state.selected[this.state.selected.length-1];
         let parent = {};
         const items = path.reduce(function(items, key) {
             parent = items[key];
@@ -42,9 +47,9 @@ Browser = React.createClass({
             </div>
 
 
-            <SlideTransition depth={path.length} selectedId={this.state.selectedId} className="items-container">
+            <SlideTransition depth={path.length} selectedId={selectedId} className="items-container">
                 {items.map(function(item, index) {
-                    return <BrowserItem item={item} index={index} selectedId={this.state.selectedId} callbackNavDown={this.navDown}></BrowserItem>
+                    return <BrowserItem item={item} index={index} selectedId={selectedId} callbackNavDown={this.navDown}></BrowserItem>
                 }.bind(this))}
             </SlideTransition>
 
